4524d098819c6c61b1d98a995c49a128f38e6038
Sunkanmi Olawuwo <solawuwo@axxess.com>
2022-01-05 18:59:36 +0100

    Full implementation

---
+++ GmailAPI/ApiHelper/Gmail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GmailAPI.ApiHelper
+{
+    internal class Gmail
+    {
+        public string From { get; set; }
+
+        public string To { get; set; }
+
+        public string Body { get; set; }
+
+        public DateTime MailDateTime { get; set; }
+
+        public List<string> Attachements { get; set; }
+
+        public string MsgID { get; set; }
+    }
+}
